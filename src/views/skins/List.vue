501b03e9d1e4ad785c53c2cea73c6612a2d1bd13
user <joshuaroggen@gmail.com>
2023-10-06 13:24:03 -0700

    whores

--- src/views/skins/List.vue
+++ src/views/skins/List.vue
@@ -4,14 +4,18 @@
       <header class="card-header">
         <h1 class="card-title">Skins</h1>
       </header>
-
       <article class="card-body">
         <label for="sortSelect">Sort by:</label>
         <select id="sortSelect" v-model="sortCriteria">
           <option value="alphabetical">Alphabetical</option>
-          <option value="weaponType">Weapon Type</option>
+          <option value="theme">Theme</option>
         </select>
-        <div v-if="isLoading" class="spinner-border" role="status">
+        <label for="themeSelect">Filter by Theme:</label>
+        <select id="themeSelect" v-model="selectedTheme">
+          <option value="All">All Themes</option>
+          <option v-for="theme in themes" :key="theme.uuid" :value="theme.uuid">{{ themeNameMap[theme.uuid] }}</option>
+        </select>
+          <div v-if="isLoading" class="spinner-border" role="status">
           <span class="visually-hidden">Loading...</span>
         </div>
         <div v-else-if="sortedSkins.length === 0">
@@ -27,21 +31,26 @@
             </div>
           </div>
         </div>
+        <div v-if="loadingError" class="alert alert-danger" role="alert">
+          Error loading data. Please try again later.
+        </div>
       </article>
     </section>
   </main>
 </template>
 
-
 <script>
 import axios from 'axios';
-
 export default {
   data() {
     return {
       skins: [],
       isLoading: true,
       sortCriteria: 'alphabetical',
+      selectedTheme: 'All',
+      themes: [],
+      themeNameMap: {},
+      loadingError: false, 
     };
   },
   created() {
@@ -51,11 +60,18 @@ export default {
     sortedSkins() {
       let sortedSkins = [...this.skins];
       if (this.sortCriteria === 'alphabetical') {
-        sortedSkins.sort((a, b) => a.displayName.localeCompare(b.displayName));
-      } else if (this.sortCriteria === 'weaponType') {
         sortedSkins.sort((a, b) => {
-          return a.weaponType.localeCompare(b.weaponType) || a.displayName.localeCompare(b.displayName);
+          const displayNameA = a.displayName || '';
+          const displayNameB = b.displayName || '';
+          return displayNameA.localeCompare(displayNameB);
         });
+      } else if (this.sortCriteria === 'theme') {
+        sortedSkins.sort((a, b) => {
+          return a.themeUuid.localeCompare(b.themeUuid) || a.displayName.localeCompare(b.displayName);
+        });
+      }
+      if (this.selectedTheme !== "All") {
+        sortedSkins = sortedSkins.filter(skin => skin.themeUuid === this.selectedTheme);
       }
       if (!this.skins || this.skins.length === 0) {
         return [];
@@ -70,16 +86,25 @@ export default {
         .then((response) => {
           this.skins = response.data.data;
           this.isLoading = false;
+          this.themes = [...new Set(response.data.data.map(skin => skin.themeUuid))];
+
+          axios.get('https://valorant-api.com/v1/themes')
+            .then((themeResponse) => {
+              themeResponse.data.forEach((theme) => {
+                this.themeNameMap[theme.uuid] = theme.displayName;
+              });
+            })
+            .catch((themeError) => {
+              this.loadingError = true; 
+            });
         })
         .catch((errors) => {
-          console.log(errors);
-          this.isLoading = false;
+          this.loadingError = true; 
         });
     },
   },
 };
 </script>
-
 <style scoped>
 .card-image {
   max-width: 80%;
